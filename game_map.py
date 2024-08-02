c3b581d8daa41c56dd33ebf4d2491425ab7d77d3
AtlasWongy <atlasyjyj@gmail.com>
2024-08-02 21:31:32 +0800

    Week 5 Update

--- game_map.py
+++ game_map.py
@@ -10,10 +10,9 @@ if TYPE_CHECKING:
 
 
 class GameMap:
-    def __init__(self, width: int, height: int):
+    def __init__(self, width: int, height: int, entities: Iterable[Entity]) -> None:
         self.width, self.height = width, height
-        # self.entities = set(entities)
-        # entities: Iterable[Entity]
+        self.entities = set(entities)
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
         self.visible = np.full((width, height), fill_value=False, order="F")
         self.explored = np.full((width, height), fill_value=False, order="F")
